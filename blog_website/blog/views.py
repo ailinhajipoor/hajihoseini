9eea37828a17ed41228e871f33b2f133923d3a85
ailinhajipoor <ailin.hajipoor@gmail.com>
2022-06-07 16:08:27 +0430

    post detail

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
 
@@ -8,3 +9,9 @@ def post_list_view(request):
     posts_list = Post.objects.all()
 
     return render(request, 'blog/posts_list.html', {'posts_list': posts_list})
+
+
+def post_detail_view(request,pk):
+
+    post = Post.objects.get(pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})

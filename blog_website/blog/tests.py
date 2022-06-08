5418ef001375458b4c04e74ff4e39fd58f1d4d04
ailinhajipoor <ailin.hajipoor@gmail.com>
2022-06-08 17:31:42 +0430

    end test blog

--- blog_website/blog/tests.py
+++ blog_website/blog/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from .models import Post
+from django.contrib.auth.models import User
+from django.shortcuts import reverse
+
 
 # Create your tests here.
+class BlogPostTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="ailin")
+        self.post = Post.objects.create(
+            title="Post1",
+            text="this is the description of post 1",
+            status=Post.STATUS_CHOICES[0],
+            author=self.user,
+        )
+
+    def test_post_list_url(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_view_by_name(self):
+        response = self.client.get(reverse('posts_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_title_on_blog_page(self):
+        response = self.client.get(reverse('posts_list'))
+        # self.assertContains(response,"Post1")
+        self.assertContains(response, self.post.title)
+
+    def test_post_detail_url(self):
+        response = self.client.get(f'/blog/{self.post.id}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_detail_view_by_name(self):
+        response = self.client.get(reverse('posts_detail', args=[self.post.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_details_on_blog_detail_page(self):
+        response = self.client.get(f'/blog/{self.post.id}/')
+        self.assertContains(response, self.post.title)
+        self.assertContains(response, self.post.text)
+
+    def test_post_details_on_blog_detail_page_by_name(self):
+        response = self.client.get(reverse('posts_detail', args=[self.post.id]))
+        self.assertContains(response, self.post.title)
+        self.assertContains(response, self.post.text)
+
+    def test_status_404_if_post_id_exist(self):
+        response = self.client.get(reverse('posts_detail', args=[2]))
+        self.assertEqual(response.status_code, 404)
--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
-from  django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -13,10 +14,14 @@ def post_list_view(request):
 
 
 def post_detail_view(request, pk):
-    try:
-        post = Post.objects.get(pk=pk)
-    except ObjectDoesNotExist:
-        post = None
-        print("Excepted")
-        print()
+    # try:
+    #     post = Post.objects.get(pk=pk)
+    # except ObjectDoesNotExist:
+    #     post = None
+    #     print("Excepted")
+    #     print()
+    # return render(request, 'blog/post_detail.html', {'post': post})
+
+    post = get_object_or_404(Post,pk=pk)
+
     return render(request, 'blog/post_detail.html', {'post': post})

81d10904f71472046e6f192269ab911b8334dae5
sinabakhshandeh <sina.bakhshandeh.t@gmail.com>
2022-06-15 17:17:28 +0430

    end of login and sign up

--- blog_website/config/settings.py
+++ blog_website/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
     'blog',
 ]
 
@@ -126,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'posts_list'
+LOGOUT_REDIRECT_URL = 'login'
